962d1ddd17b1dacad76bfd164b8a7b68ab3d36c9
Stefano Anzellotti <anzellot@node052.eth.cluster>
2017-08-19 00:11:00 -0400

    update

---
+++ MVPDsearchlight/MVPDsearchlight_masterwrapper.m
@@ -0,0 +1,24 @@
+
+%% Multivariate analyses - Revision
+nSubjects = 10;
+scriptDir = '/mindhive/saxelab3/anzellotti/facesVoices_art2/MVPDtests/searchlight/products';
+mvpcDir = '/mindhive/saxelab3/anzellotti/github_repos/mvpd/';
+
+ parameters.slurm.name = 'mvpc_sl_rev';
+ parameters.slurm.cores_per_node = 4; 
+ parameters.slurm.cpus_per_task = 1; 
+ parameters.slurm.time = 5; % time in days
+
+ parameters.slurm.mem_per_cpu = 16000;
+ parameters.slurm.email = 'anzellot@mit.edu';
+
+MVPDsearchlight_scriptGenerator(nSubjects,scriptDir,mvpcDir,parameters);
+ 
+ %% Submit to queue
+ 
+system(sprintf('cd %s',scriptDir));
+ for iJob = 1:3
+ system(sprintf('sbatch sbatch_newMvpc_parallel_searchlight_rev_%d.sh',iJob));
+ end
+
+
